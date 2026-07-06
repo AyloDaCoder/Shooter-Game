b877963fade46789900544f89937624ca3802ae8
AyloDaCoder <titolopes2711@gmail.com>
2026-07-06 18:05:43 -0400

    First version
    Yey first versions!!

---
+++ 201418/config.py
@@ -0,0 +1,24 @@
+# Constantes 
+Alto = 480
+Ancho = 640
+Titulo = "Proyecto Shooter"
+
+#colores
+White = (255, 255, 255)
+Black = (0, 0, 0)
+Back = (136, 3, 252)
+
+#Images
+Player_IMG = "src/Player.png"
+Enemy_IMG = "src/Enemy.png"
+Background_IMG = 'src/background.png'
+
+# SOUNDS
+
+# PARAMETROS INICIALES
+VIDAS = 5
+FALLOS = 0
+PUNTOS = 0
+FPS = 60
+
+run = True
